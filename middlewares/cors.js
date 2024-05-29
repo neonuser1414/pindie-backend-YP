051ee939e6d9595358cd475a43a9c484c052c44f
neonuser1414 <constant8765@gmail.com>
2024-05-29 18:38:48 +0300

    Changed CORS policy

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,16 +3,17 @@ const allowedCors = [
     'https://practicum.yandex.ru',
     'https://students-projects.ru',
     'http://localhost:3000',
-    'http://localhost:3001'
+    'http://localhost:3001',
+    'https://pindiefrontkm.nomorepartiesco.ru'
 ];
 
 function cors(req, res, next) {
     const { origin } = req.headers; // Смотрим, кто стучится к серверу
-    if (allowedCors.includes(origin)) { // Если это наш друг
-        res.header('Access-Control-Allow-Origin', origin);
-        res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    if (allowedCors.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
     }
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
     next();
 }
 
